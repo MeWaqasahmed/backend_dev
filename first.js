b460ddb8942007f3975aa89fed94f9544f696edf
MeWaqasahmed <mwahmed009@gmail.com>
2025-05-22 21:15:42 +0500

    basic + modules in javascripts

---
+++ first.js
@@ -0,0 +1,4 @@
+const {add,subtract,greet} = require("./math");
+console.log(add(6, 8));
+// console.log(math)
+console.log(greet("waqas"))
---
+++ math.js
@@ -0,0 +1,8 @@
+function add(a, b) { return a + b }
+function subtract(a, b) { return a - b }
+function greet(name) { return `Hello, ${name}!` }
+module.exports={
+  add,
+  subtract,
+  greet
+} 
