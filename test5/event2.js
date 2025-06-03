039efdfe4af0d9e6e71ead543fc1fb534e2cb45d
MeWaqasahmed <mwahmed009@gmail.com>
2025-06-03 06:48:08 +0500

    playing/learning streams in file handling

--- test5/event2.js
+++ test5/event2.js
@@ -1,57 +1,78 @@
 const eventEmitter = require("events");
+const fs = require("fs");
 const emitter = new eventEmitter();
-let loginCounter = 0;
-let logoutCounter = 0;
-let purchaseCounter = 0;
-let profileCounter = 0;
+const user = {
+   loginCounter : 0,
+   logoutCounter : 0,
+   purchaseCounter : 0,
+   profileCounter : 0
+}
 let lastProduct = '';
-
-emitter.on("login", (name) => {
-   console.log(`${name} is just logged in `);
-   loginCounter++;
-   
-})
-emitter.on("logout", (name) => {
-   
-   console.log(`${name} is just logged out `);
-   logoutCounter++;
-   
-})
-
-emitter.on("purchase", (args) => {
-   
-   console.log(`${args.customerName} purchased ${args.product}`);
-  purchaseCounter++;
-  lastProduct = args.product;
-   
-})
-emitter.on("profile", (userName) => {
-   
-   console.log(`${userName} update his profile`);
-   profileCounter ++;
-   
-})
-
-emitter.emit("login", "waqas ahmed");
-emitter.emit("login", "waqas ahmed");
-
-emitter.emit("logout", "Qazi")
-emitter.emit("logout", "Qazi")
-
-emitter.emit("profile", "waqas ahmed")
-emitter.emit("profile", "waqas ahmed")
-
-emitter.emit("purchase", {
-   customerName: "waqas ahmed",
-   product: "apple"
-})
-emitter.emit("purchase", {
-   customerName: "waqas ahmed",
-   product: "apple"
-})
-console.log(`${loginCounter} time${loginCounter > 1 ? 's' : ''} user log in `)
-console.log(`${logoutCounter} time${logoutCounter > 1 ? 's' : ''}  user logged out `)
 try {
-  console.log(`${purchaseCounter} time${purchaseCounter > 1 ? 's' : ''}  customer purchased ${lastProductm}`)
-} catch (error) { console.log(`catch error ${error}`)}
-console.log(`${profileCounter} time${profileCounter>1 ? 's': ''}  update his profile `)
+  const logFile = "eventlog.json";
+  
+  if (fs.existsSync(logFile))
+  { 
+    const data = fs.readFileSync(logFile, "utf-8")
+    Object.assign(user, JSON.parse(data))
+  };
+
+
+  function saveCounts() {
+    fs.writeFileSync(logFile, JSON.stringify(user), null, 2);
+  }
+
+
+
+  emitter.on("login", (name) => {
+    console.log(`${name} is just logged in `);
+    user.loginCounter++;
+    saveCounts();
+   
+  })
+
+  emitter.on("logout", (name) => {
+    console.log(`${name} is just logged out `);
+    user.logoutCounter++;
+    saveCounts();
+   
+  })
+
+  emitter.on("purchase", (args) => {
+    console.log(`${args.customerName} purchased ${args.product}`);
+   user.purchaseCounter++;
+    lastProduct = args.product;
+    saveCounts();
+   
+  })
+
+  emitter.on("profile", (userName) => {
+    console.log(`${userName} update his profile`);
+    user.profileCounter++;
+    saveCounts();
+   
+  })
+
+
+  emitter.on("summary", () => {
+    console.log(`Summary of user activity:`);
+    
+    console.log(`${user.loginCounter} time${user.loginCounter > 1 ? 's' : ''} user log in `)
+    console.log(`${user.logoutCounter} time${user.logoutCounter > 1 ? 's' : ''}  user logged out `)
+    console.log(`${user.purchaseCounter} time${user.purchaseCounter > 1 ? 's' : ''}  customer purchased ${lastProduct}`)
+    console.log(`${user.profileCounter} time${user.profileCounter > 1 ? 's' : ''}  update his profile `)
+    saveCounts();
+  })
+  
+  emitter.emit("login", "waqas ahmed");
+  emitter.emit("logout", "Qazi")
+  emitter.emit("profile", "waqas ahmed")
+  emitter.emit("purchase", {
+    customerName: "waqas ahmed",
+    product: "apple"
+  
+  })
+  emitter.emit("summary")
+} catch (error) {
+  console.log(error)
+}
---
+++ test5/stream.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 4050;
+
+try {
+  const myServer = http.createServer((req, res) => {
+    
+
+    // downloading files in a bad way
+    
+    // fs.readFile("sample.txt", (error, data) => {
+      
+    //   if (error) {
+    //     console.log(error)
+    //     res.statusCode = 500;
+    //     res.end("internal server error");
+    //   }
+      
+    //   else {
+    //     res.writeHead(200, { "content-type": "text/plain" })
+    //     res.end(data);
+    //   }
+    // });
+    
+    // Reading a file in a good way 
+
+    const readStream = fs.createReadStream("sample2.txt", {highwaterMark: 16});
+    const writeStream = fs.createWriteStream("sample3.txt")
+    readStream.on("data", (chunked_data) => {
+      writeStream.write(chunked_data);
+    
+    })
+
+
+  
+  })
+  myServer.listen(PORT, () => {
+    console.log(`server in listening at port ${PORT}`)
+  })
+} catch (error) {
+  console.log(`the error is ${error}`)
+  
+}
