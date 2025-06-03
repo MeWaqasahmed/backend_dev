3e2a469968bea40c322e085a4e05262e43b66ed9
MeWaqasahmed <mwahmed009@gmail.com>
2025-06-03 16:56:17 +0500

    playing/learning streams in file handling

--- test5/stream_test.js
+++ test5/stream_test.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
+const http = require("http");
+const PORT = 8080;
+const myServer = http.createServer((req, res) => {
+
+  res.writeHead(200, { "content-type": "text/plain" })
 
   
-const readStream = fs.createReadStream("sample2.txt");
-const writeStream = fs.createWriteStream("sample3.txt");
-  readStream.pipe(writeStream);
+  const readStream = fs.createReadStream("sample2.txt");
+  // const writeStream = fs.createWriteStream("sample3.txt");
+    readStream.pipe(res);
+  
+    readStream.on("data", (chunk) => {
+      console.log(chunk);
+    })
+})
+myServer.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+})
 
-  readStream.on("data", (chunk) => {
-    
-    // console.log(chunk.toString());
-    console.log(chunk);
-  })
+  
 
 
   
